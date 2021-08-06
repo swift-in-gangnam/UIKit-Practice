13260579e26c56e1900a95e783a644cb3a6daed8
gamermook <mook911205@gmail.com>
2021-08-06 22:41:36 +0900

    210806-mook bookmanager edit

---
+++ [1]bookManagementProgram/mook/Book.swift
@@ -0,0 +1,19 @@
+//
+//  Book.swift
+//  BookManager
+//
+//  Created by 강은묵 on 2021/08/06.
+//
+
+struct Book {
+    var name:String
+    var genre:String
+    var author:String
+    
+    func bookPrint(){
+        print("Name : \(name)")
+        print("Name : \(genre)")
+        print("Name : \(author)")
+        print("----------")
+    }
+}
